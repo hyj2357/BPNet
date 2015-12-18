587b4eb817418a37651601c2dceaec5ab186a74e
hyj2357 <976350901@qq.com>
2015-12-18 12:06:51 +0800

    m

--- BPNet/src/com/BPNET/impl/BPNetwork.java
+++ BPNet/src/com/BPNET/impl/BPNetwork.java
@@ -5,15 +5,15 @@ import java.util.Random;
 import com.BPNET.Net;
 
 public class BPNetwork implements Net{
-   private double input[],output[];
-   private double X[][];
-   private double weight[][][];
-   private double deltaWeight[][][];
-   private int layers;
-   private int nodeNumOfLayer[];
-   private double Y[];
-   private double d[][];
-   private static final double STEP = 0.2;    //    
+   private double input[],output[];    //输入输出.
+   private double X[][];               //每层的输出,例如 X[0][1],表示第0层的第1个神经元的输出.并且 X[k][i] = Xi(k) = f(Ui(k)) .Xi.
+   private double weight[][][];        //每两层之间的各个神经元连接的权值 ,例如 weight[0][1][2],表示第0层第1个神经元到第1层第2个神经元之间连接输入输出的权值 .Wij
+   private double deltaWeight[][][];   //每次调整权值改变量.ΔWij.
+   private int layers;                 //该神经网络层数.
+   private int nodeNumOfLayer[];       //每一层的神经元数.
+   private double Y[];                 //教师信号.Yi.
+   private double d[][];               //每一层每个神经元的d值,用于反向传播.di(k).
+   private static final double STEP = 0.2;    //学习步数,η.    
 
 public BPNetwork(int layers,int nodeNumOfLayer[]){
 	   if(layers<2){
@@ -46,6 +46,9 @@ public BPNetwork(int layers,int nodeNumOfLayer[]){
    public void learn(double trainData[][],double tSignal[][]){
 	   
    }
+   /**
+    * 初始化权重矩阵
+    */
    private void initWeight(){
 	   Random random = new Random(19881211);
 	   for(int i=0;i<this.weight.length;i++)
@@ -55,36 +58,61 @@ public BPNetwork(int layers,int nodeNumOfLayer[]){
 				   this.weight[i][j][k] = o>0.5?o:-o;
 			   }
    }
+   /**
+    * 进行单个数据学习.该方法只能被learn方法调用.
+    * @param data
+    */
    private void learnOne(double data[]){
-      this.forward_propagating();
-      this.backard_propagating();
+      this.forward_propagating();    //正向传播
+                                     //与教师信号比对
+      this.backard_propagating();    //如果出现结果不符,进行反向传播.
    }
    /**
     * 正向传播过程
     * 获取output
     */
-private void forward_propagating(){
-	   for(int i=0;i<this.input.length;i++)
-		   X[0][i] = Tools.sigmod(input[i]);
-	   for(int i=0;i<X.length;i++){
-		   for(int j=0;j<this.X[i].length;j++){
+   private void forward_propagating(){
+	   for(int i=0;i<input.length;i++)
+		   X[0][i] = Tools.sigmod(input[i]);            //计算第0层神经元的输出.X[0][i] = Xi(0) = f(Ui(0)).f(Ui(0)) = sigmod(Ui(0)).
+	   for(int k=1;k<X.length;k++){
+		   for(int j=0;j<X[k].length;j++){
 			   double U = 0;
-			   for(int k=0;k<this.X[i-1].length;k++)
-				   U += (this.X[i-1][k]*this.weight[i-1][k][j]);
-			   this.X[i][j] = Tools.sigmod(U);
+			   for(int i=0;i<X[k-1].length;i++)
+				   U += (X[k-1][i]*weight[k-1][i][j]);  //Ui(k)=(j)Σ(Wij*Xj(k-1)).
+			   X[k][j] = Tools.sigmod(U);               //X[k][i] = Xi(k) = f(Ui(k)).
 		   }
 	   }
 	   for(int i=0;i<output.length;i++)
-		   this.output[i] = this.X[X.length-1][i];
+		   output[i] = X[X.length-1][i];
    }
    /**
     * 反向传播
     * 并修改每层权值
     */
    private void backard_propagating(){
-	   for(int i=0;i<d[d.length-1].length;i++)
-		   d[d.length-1][i] = (X[X.length-1][i]-Y[i])*X[X.length-1][i]*(1-X[X.length-1][i]);
-	   for(int k=this.layers-1)
+	   //求解公式
+	   //ΔWij = -η*di(m)*Xj(m-1).( k=m AND di(m)=(Xi(m)-Yi)*Xi(m)*(1-Xi(m)) )
+	   //ΔWij = -η*di(k)*Xj(k-1).( k<m AND di(k)=)
+	   //初始化第m层(m为输出层)d与权值
+	   int m = X.length-1;
+	   for(int i=0;i<d[m].length;i++)
+		   d[m][i] = (X[m][i]-Y[i])*X[m][i]*(1-X[m][i]); //di(m)=(Xi(m)-Yi)*Xi(m)*(1-Xi(m))
+	   for(int j=0;j<X[X.length-2].length;j++){
+		   for(int i=0;i<X[X.length-1].length;i++){
+			   weight[layers-2][j][i] += (-1)*STEP*d[d.length-1][i]*X[X.length-2][j];        // ΔWij = -η*di(k)*Xj(k-1)
+		   }
+	   }
+	   for(int k=this.layers-2;k>0;k--){
+		   for(int i=0;i<X[k].length;i++){
+			   double sum_wd = 0;
+			   for(int l=0;l<X[k+1].length;l++)
+				   sum_wd += (weight[k][i][l]*d[k+1][l]);
+			   d[k][i] =  X[k][i]*(1-X[k][i])*sum_wd;
+		   }
+		   for(int j=0;j<X[k-1].length;j++)
+			   for(int i=0;i<X[k].length;i++)
+				   weight[k-1][j][i] += (-1)*STEP*d[k][i]*X[k-1][j];			   		   
+	   }
    }
 public double[] getInput() {
 	return input;
